1204197636507ed5d94745d987f6ba599566be75
pak11273 <pak11273@gmail.com>
2021-03-11 14:19:43 -0600

    adds spring framework

---
+++ src/main/java/com/labdaschool/web39/demo/Web39JavaCountriesApplication.java
@@ -0,0 +1,13 @@
+package com.labdaschool.web39.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web39JavaCountriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web39JavaCountriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/labdaschool/web39/demo/Web39JavaCountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labdaschool.web39.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web39JavaCountriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
